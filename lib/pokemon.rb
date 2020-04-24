c988d52e06779ca911c5fc4bd5b9ccdde21f5598
Ray Alva <rayalva407@gmail.com>
2020-04-24 23:31:39 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,14 @@
 class Pokemon
+  attr_accessor :name, :type
+  attr_reader :id, :db
+  
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+  
+  
+  
 end
