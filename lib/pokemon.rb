2da00b0777861f68a5fa7d9f625d9eb6024de3df
Ray Alva <rayalva407@gmail.com>
2020-04-25 00:53:08 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
       SELECT * FROM pokemon WHERE id = (?);
       SQL
     pokemon = db.execute(sql, [id])
-    Pokemon.new(id, pokemon[1], pokemon[2], pokemon[3], db )
+    Pokemon.new(pokemon[1], pokemon[2], pokemon[3])
   end
   
 end
