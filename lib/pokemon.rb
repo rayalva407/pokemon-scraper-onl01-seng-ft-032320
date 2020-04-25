c6bedc96006624d2239ec478b0a4da9dd8e6cb67
Ray Alva <rayalva407@gmail.com>
2020-04-25 00:48:40 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,6 +20,8 @@ class Pokemon
       pokemon = db.execute("SELECT * FROM pokemon (name, type) WHERE id=?", id)
       new_pokemon = self.new(pokemon)
       new_pokemon.id = pokemon[0][0]
+      new_pokemon.name = pokemon[0][1]
+      new_pokemon.type = pokemon[0][2]
       
   end
   
