36f1a5d6ba49269e03a3cb01b6f1d8972f95ccce
Ray Alva <rayalva407@gmail.com>
2020-04-25 01:09:33 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -17,7 +17,7 @@ class Pokemon
   end
   
   def self.find(id, db)
-    pokemon = db.execute("SELECT * FROM pokemon WHERE id=")
+    pokemon = db.execute("SELECT * FROM pokemon WHERE id = ?", id)
   end
   
 end
